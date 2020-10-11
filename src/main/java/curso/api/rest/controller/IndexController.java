c11c9af3a0b68372115e8dc8fd4b60f97128720b
Anailson Ribeiro <arsantos315@gmail.com>
2020-10-11 13:57:35 -0300

    RestFullDelete

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,4 +68,12 @@ public class IndexController {
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
 	
+	//DELETE REGISTROS
+	@DeleteMapping(value = "/{id}" ,produces = "application/json")
+	public String delete(@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "Registro Deletado";
+	}
 }
