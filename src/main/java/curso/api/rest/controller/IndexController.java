83079efee8902e6f016192a6394856b2bdf2c1ef
Anailson Ribeiro <arsantos315@gmail.com>
2020-10-11 13:17:32 -0300

    ServicoPostmanCadastro

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,17 @@ public class IndexController {
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	//SALVANDO OS DADOS CRIADO NO POSTMAN
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		//GRAVAR NO BANCO DE DADOS O RETORNO 
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
 	}
 	
 	
