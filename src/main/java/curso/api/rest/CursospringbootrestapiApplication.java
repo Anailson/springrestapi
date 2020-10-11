d5d2efc10c161aaca1a7a24cebff909d028141b6
Anailson Ribeiro <arsantos315@gmail.com>
2020-10-11 16:18:19 -0300

    CrossOriginConfiguracaoCentralizada

--- src/main/java/curso/api/rest/CursospringbootrestapiApplication.java
+++ src/main/java/curso/api/rest/CursospringbootrestapiApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*ANOTAÇÕES NECESSÁRIOS PARA O PROJETO SPRING REST API*/
 @SpringBootApplication
@@ -19,10 +21,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class CursospringbootrestapiApplication {
+public class CursospringbootrestapiApplication  implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootrestapiApplication.class, args);
 	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario**/")//LIBERANDO AÇÕES PARA O addMapping
+		.allowedOrigins("*")//LIBERANDO TODAS AS FUNÇÕE DA API
+		.allowedOrigins("*");//LIBERANDO O MAPEAMENTO DE USUARIOS PARA TODAS AS ORIGENS DA API
+	}
+	
+	
 
 }
