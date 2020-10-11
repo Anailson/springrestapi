d80f334646e07f1f3ca2d2c5bb962e542ccfa692
Anailson Ribeiro <arsantos315@gmail.com>
2020-10-11 19:36:27 -0300

    TestandoAplicacaoPostman

--- src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,13 +37,14 @@ public class WebConfigSecurity  extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 		
 		//MAPEIA A URL DE LOGOUT E INVALIDA O USUARIO
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 		
 		
-		//FILTRA REQUISIÇÕES DE LOGING
-		
-		
-		//FILTRA DEMAIS REQUISIÇÕES PARA VERIFICAR A PRESENÇA DO TOKEN JWT NO HEADER
+		// Filtra requisições de login para autenticação
+		.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+				
+		// Filtra demais requisições para verificar a presença do TOKEN JWT no HEADER HTTP
+		.addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);		
 	
 		
 	}
